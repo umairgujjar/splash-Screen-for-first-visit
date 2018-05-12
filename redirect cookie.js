1f5b4a4849c4cf402d0644e80b68b4924dc9dd01
umairgujjar <37044171+umairgujjar@users.noreply.github.com>
2018-05-12 17:16:41 +0500

    Create redirect cookie.js

---
+++ redirect cookie.js
@@ -0,0 +1,31 @@
+<script type="text/javascript">
+function redirect(){
+var thecookie = readCookie('doRedirect');
+if(!thecookie){window.location = 'https://websites-developer.com';
+}}
+  function createCookie(name,value,days){
+    if (days){
+      var date = new Date();
+      date.setTime(date.getTime()+(days*24*60*60*1000));
+      var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+  }
+  function readCookie(name){
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++)
+    {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0)
+        return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  }
+  window.onload = function(){
+    redirect();
+   createCookie('doRedirect','true','999');
+}
+</script>
